6f6296c09113436b4c7fd2747b856f8e69c38604
Jan Krivec <jan.krivec98@gmail.com>
2023-05-18 13:50:51 +0200

    Popravek

--- api/src/controller/journal/journal.service.ts
+++ api/src/controller/journal/journal.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { AbiItem } from 'web3-utils'
 import {AlchemyWeb3, createAlchemyWeb3} from "@alch/alchemy-web3";
 import {ConfigService} from "@nestjs/config";
 import { ethers } from 'ethers';
-
-const contract = require('../../contracts/journal/journalNFT.json')
+import {journalContract} from '../../contracts/journal/journalNFT';
 
 
 @Injectable()
@@ -12,10 +12,8 @@ export class JournalService {
     private PUBLIC_KEY: string = this.configService.get<string>('ACCOUNT_PUBLIC_KEY');
     private PRIVATE_KEY: string = this.configService.get<string>('ACCOUNT_PRIVATE_KEY');
     private web3: AlchemyWeb3 = createAlchemyWeb3(this.API_URL);
-    private contract
 
-    private contractAddress: string = "0x969F6F767d2c20B4a93Bf6088188D3E1E15D9fA2";
-    private nftContract = new this.web3.eth.Contract(contract.abi, contract.contractAddress);
+    private nftContract = new this.web3.eth.Contract(journalContract.abi as AbiItem[], journalContract.contractAdress);
 
 
     constructor(private configService: ConfigService) {}
